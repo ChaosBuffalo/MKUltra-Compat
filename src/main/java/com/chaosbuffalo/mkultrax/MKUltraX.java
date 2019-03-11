f4684a05bedf413870fc4142350adab7ad5b70cd
Lee Harrison <ralekdev@gmail.com>
2019-03-10 22:59:26 -0700

    Inject version number into the jar

--- src/main/java/com/chaosbuffalo/mkultrax/MKUltraX.java
+++ src/main/java/com/chaosbuffalo/mkultrax/MKUltraX.java
@@ -21,7 +21,7 @@ public class MKUltraX
 {
     public static final String MODID = "mkultrax";
     public static final String NAME = "MK Ultra Compat";
-    public static final String VERSION = "0.06";
+    public static final String VERSION = "@VERSION@";
 
     public static IceAndFireIntegration iceAndFire;
     public static LycanitesIntegration lycanites;
